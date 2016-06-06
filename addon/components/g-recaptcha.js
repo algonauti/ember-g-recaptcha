6fcb274df4f6c6e9c1f1d4405421eada55fe651b
Davide Papagni <davide@algonauti.com>
2016-06-06 17:59:17 +0200

    g-recaptcha component implementation

---
+++ addon/components/g-recaptcha.js
@@ -0,0 +1,61 @@
+import Ember from 'ember';
+import Configuration from '../configuration';
+
+export default Ember.Component.extend({
+
+  sitekey: Configuration.siteKey,
+
+  tabindex: Ember.computed.alias('tabIndex'),
+
+  renderReCaptcha() {
+    if (Ember.isNone(window.grecaptcha)) {
+      Ember.run.later(() => {
+        this.renderReCaptcha();
+      }, 500);
+    } else {
+      let container = this.$()[0];
+      let properties = this.getProperties(
+        'sitekey',
+        'theme',
+        'type',
+        'size',
+        'tabindex'
+      );
+      let parameters = Ember.merge(properties, {
+        callback: this.get('successCallback').bind(this),
+        'expired-callback': this.get('expiredCallback').bind(this)
+      });
+      let widgetId = window.grecaptcha.render(container, parameters);
+      this.set('widgetId', widgetId);
+    }
+  },
+
+  successCallback(reCaptchaResponse) {
+    let action = this.get('onSuccess');
+    if (Ember.isPresent(action)) {
+      action(reCaptchaResponse);
+    }
+  },
+
+  expiredCallback() {
+    let action = this.get('onExpired');
+    if (Ember.isPresent(action)) {
+      action();
+    } else {
+      if (Ember.isPresent(this.set('widgetId'))) {
+        window.grecaptcha.reset(this.set('widgetId'));
+      }
+    }
+  },
+
+
+  // Lifecycle Hooks
+
+  didInsertElement() {
+    this._super(...arguments);
+    Ember.run.next(() => {
+      this.renderReCaptcha();
+    });
+  }
+
+});
---
+++ app/components/g-recaptcha.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-g-recaptcha/components/g-recaptcha';
