06fdf7e4260ac3218c1777957c0f90d19a09b9ba
Davide Papagni <davide@algonauti.com>
2016-06-06 17:58:43 +0200

    load configuration object from ENV

---
+++ app/initializers/ember-g-recaptcha.js
@@ -0,0 +1,13 @@
+import ENV from '../config/environment';
+import Configuration from 'ember-g-recaptcha/configuration';
+
+export default {
+
+  name: 'ember-g-recaptcha',
+
+  initialize: function(registry) {
+    const config = ENV.gReCaptcha || {};
+    Configuration.setProperties(config);
+  }
+
+}
