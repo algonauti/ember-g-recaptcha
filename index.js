5786a1edea1dc54102510659315d600f30b9c300
Davide Papagni <davide@algonauti.com>
2016-06-10 11:38:44 +0200

    avoid JSHint strict mode error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-g-recaptcha',
 
   contentFor: function(type, config) {
-    let content = '';
+    var content = '';
     if (type === 'head') {
       let src = 'https://www.google.com/recaptcha/api.js?render=explicit';
       content = `<script type="text/javascript" src="${src}"></script>`;
