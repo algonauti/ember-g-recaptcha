d3817536c04af2271e0c0cf8c0104d4d7895fcf3
Nick Anderson <nick.anderson@omadahealth.com>
2018-10-03 20:46:05 -0700

    Update packages and refactor how reCaptcha is loaded.
    With these changes, an `onRender` callback can be passed in to the `g-recaptcha` component. The `onRender` callback will be triggered after the reCaptcha's `render` function is called.
    
    Also update node packages for security reasons surfaced via `npm audit`.

--- index.js
+++ index.js
@@ -3,13 +3,4 @@
 
 module.exports = {
   name: 'ember-g-recaptcha',
-
-  contentFor: function(type, config) {
-    var content = '';
-    if (type === 'body-footer') {
-      var src = config.gReCaptcha.jsUrl || 'https://www.google.com/recaptcha/api.js?render=explicit';
-      content = '<script type="text/javascript" src="'+src+'" async></script>';
-    }
-    return content;
-  }
 };
