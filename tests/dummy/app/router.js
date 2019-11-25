ba7ae730bb5ce7ef4b2c0e1e7ded68bfb77a7ece
Davide Papagni <davide@algonauti.com>
2019-11-25 11:23:30 +0100

    upgrade to ember-cli 3.14.0

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,10 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
   this.route('google-defaults');
@@ -13,5 +13,3 @@ Router.map(function() {
   this.route('compact-size');
   this.route('invisible-mode');
 });
-
-export default Router;
