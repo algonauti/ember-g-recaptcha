375f0c9c966afb19ae497c73cce23725d40b817e
Sinan Keskin <sinan@kesk.in>
2022-02-26 14:11:39 +0300

    v3.16.2...v3.20.2

--- config/ember-try.js
+++ config/ember-try.js
@@ -23,6 +23,14 @@ module.exports = async function() {
         }
       },
       {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.20.5'
+          }
+        }
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
@@ -46,14 +54,17 @@ module.exports = async function() {
           }
         }
       },
-      // The default `.travis.yml` runs this scenario via `yarn test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
       {
-        name: 'ember-default',
+        name: 'ember-default-with-jquery',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'jquery-integration': true
+          })
+        },
         npm: {
-          devDependencies: {}
+          devDependencies: {
+            '@ember/jquery': '^1.1.0'
+          }
         }
       },
       {
