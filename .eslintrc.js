84b0344303859d38dc39f8f4d9734b6df5c3c7a7
Sinan Keskin <sinan@kesk.in>
2022-02-26 15:00:06 +0300

    Update addon to latest packages and enable octane

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,11 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true,
-    },
+    requireConfigFile: true,
   },
   plugins: ['ember'],
   extends: [
